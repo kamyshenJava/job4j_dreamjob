c133e4674993e83e984ff4a53c3c182586ff7a38
kamyshenJava <kamyshen@tutanota.com>
2022-05-29 04:28:59 +0300

    4. Формы. Загрузка файла на сервер. [#504855]

--- src/main/java/ru/job4j/dreamjob/model/Candidate.java
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -8,6 +8,7 @@ public class Candidate {
     private String name;
     private String description;
     private LocalDate created;
+    private byte[] photo;
     public Candidate(String name, String description) {
         this.name = name;
         this.description = description;
@@ -43,6 +44,14 @@ public class Candidate {
 
     public void setCreated(LocalDate created) {
         this.created = created;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
     }
 
     @Override
